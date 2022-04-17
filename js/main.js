02b3815e97a7bd4ba1f6092ab74bcb6e5c5f77fc
DimonKZ <66905414+DimonKZ@users.noreply.github.com>
2022-04-18 01:10:06 +0600

    Slider

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let sliderItem = document.querySelectorAll('.slider__item');
+console.log(sliderItem);
+let slider = [];
+for (let i = 0; i < sliderItem.length; i++) {
+	slider[i] = sliderItem[i].id;
+	sliderItem[i].remove();
+}
+console.log(slider);
+
+let step = 0;
+let offset = 0;
+
+function draw() {
+	let div = document.createElement('div');
+	div.id = slider[step];
+	div.classList.add('.slider__item');
+	div.style.left = offset * 36.2 + 'rem';
+	document.querySelector('.slider-line').appendChild(div);
+}
+
+draw();
