571324097363a0d896c3d808d373965b0d7e61c7
DimonKZ <66905414+DimonKZ@users.noreply.github.com>
2022-04-22 16:09:42 +0600

    Media

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
-let sliderItem = document.querySelectorAll('.slider__item');
-console.log(sliderItem);
-let slider = [];
-for (let i = 0; i < sliderItem.length; i++) {
-	slider[i] = sliderItem[i].id;
-	sliderItem[i].remove();
-}
-console.log(slider);
+// let sliderItem = document.querySelectorAll('.slider__item');
+// console.log(sliderItem);
+// let slider = [];
+// for (let i = 0; i < sliderItem.length; i++) {
+// 	slider[i] = sliderItem[i].id;
+// 	sliderItem[i].remove();
+// }
+// console.log(slider);
 
-let step = 0;
-let offset = 0;
+// let step = 0;
+// let offset = 0;
 
-function draw() {
-	let div = document.createElement('div');
-	div.id = slider[step];
-	div.classList.add('.slider__item');
-	div.style.left = offset * 36.2 + 'rem';
-	document.querySelector('.slider-line').appendChild(div);
-}
+// function draw() {
+// 	let div = document.createElement('div');
+// 	div.id = slider[step];
+// 	div.classList.add('.slider__item');
+// 	div.style.left = offset * 36.2 + 'rem';
+// 	document.querySelector('.slider-line').appendChild(div);
+// }
 
-draw();
+// draw();
+
+/*=============================*/
+let sliderLine = document.querySelector('.slider-line');
+window.addEventListener('click', () => {
+	sliderLine.style.position = 'relative';
+	sliderLine.style.left by -36.2 + 'rem';
+});
