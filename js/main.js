c366f0faece50f05f96e57d0efcbeb561a891fce
DimonKZ <66905414+DimonKZ@users.noreply.github.com>
2022-05-04 13:50:13 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
-// let sliderItem = document.querySelectorAll('.slider__item');
-// console.log(sliderItem);
-// let slider = [];
-// for (let i = 0; i < sliderItem.length; i++) {
-// 	slider[i] = sliderItem[i].id;
-// 	sliderItem[i].remove();
-// }
-// console.log(slider);
-
-// let step = 0;
-// let offset = 0;
-
-// function draw() {
-// 	let div = document.createElement('div');
-// 	div.id = slider[step];
-// 	div.classList.add('.slider__item');
-// 	div.style.left = offset * 36.2 + 'rem';
-// 	document.querySelector('.slider-line').appendChild(div);
-// }
-
-// draw();
-
-/*=============================*/
-let sliderLine = document.querySelector('.slider-line');
-window.addEventListener('click', () => {
-	sliderLine.style.position = 'relative';
-	sliderLine.style.left by -36.2 + 'rem';
-});
+const menuBurger = document.querySelector('.menu__burger');
+if (menuBurger) {
+	const headerMenu = document.querySelector('.header__menu');
+	menuBurger.addEventListener('click', function(e) {
+		// menuBurger.classList.toggle('active');
+		headerMenu.classList.toggle('active');
+	});
+}
