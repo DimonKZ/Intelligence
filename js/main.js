45668a85f388c0b3d01a5a5bcb6fc39a1b8e06c8
DimonKZ <66905414+DimonKZ@users.noreply.github.com>
2022-05-13 08:58:31 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-const menuBurger = document.querySelector('.menu__burger');
-if (menuBurger) {
-	const headerMenu = document.querySelector('.header__menu');
-	menuBurger.addEventListener('click', function(e) {
-		menuBurger.classList.toggle('cross');
-		headerMenu.classList.toggle('active');
-	});
+function burgerMenu() {
+	const menuBurger = document.querySelector('.menu__burger');
+	if (menuBurger) {
+		const headerMenu = document.querySelector('.header__menu');
+		menuBurger.addEventListener('click', function(e) {
+			menuBurger.classList.toggle('cross');
+			headerMenu.classList.toggle('active');
+		});
+	}
 }
+
+burgerMenu();
+
+
+const leftBtn = document.querySelector('.left');
+const rightBtn = document.querySelector('.right');
+const sliderItem = document.querySelector('.slider__item');
+const sliderLine = document.querySelector('.slider-line');
+let offset = 0;
+// alert(sliderItem.offsetWidth)
+
+leftBtn.addEventListener('click', () => {
+	sliderLine.style.transform = `translateX(-${sliderItem.offsetWidth}px)`;
+})
