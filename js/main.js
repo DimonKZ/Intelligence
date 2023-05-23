c3d7a461859cf506568d33f0f2fec4399d83e52b
DimonKZ <66905414+DimonKZ@users.noreply.github.com>
2023-05-23 15:09:10 +0600

    Responsive 100%

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function burgerMenu() {
-	const menuBurger = document.querySelector('.menu__burger');
+	const menuBurger = document.querySelector('.burger-menu');
 	if (menuBurger) {
-		const headerMenu = document.querySelector('.header__menu');
+		const headerMenu = document.querySelector('div.header__nav');
 		menuBurger.addEventListener('click', function(e) {
 			menuBurger.classList.toggle('cross');
-			headerMenu.classList.toggle('active');
+			headerMenu.classList.toggle('header__nav--active');
 		});
 	}
 }
